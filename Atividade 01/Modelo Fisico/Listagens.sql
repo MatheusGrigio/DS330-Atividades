e009dad34f9d450fc52dfcaa5317b5de3105244d
Mateus De Vita <mateusdevitata@gmail.com>
2023-08-30 19:28:44 -0300

    Criar listagens de dados das tabelas

---
+++ Atividade 01/Modelo Fisico/Listagens.sql
@@ -0,0 +1,31 @@
+SELECT Veiculo.Chassi, Marca.Nome AS "Marca", Modelo.Nome AS "Modelo", Veiculo.Ano, Veiculo.Km, Veiculo.Status FROM Veiculo
+INNER JOIN modelo ON veiculo.cod_modelo=modelo.cod_modelo
+INNER JOIN marca ON modelo.Cod_Marca = marca.Cod_Marca;
+
+SELECT 
+	cliente.Nome, 
+	cliente.CPF, 
+    veiculo.Chassi, 
+    Marca.Nome AS "Marca", 
+    Modelo.Nome AS "Modelo", 
+    aluguel.Data_aluguel AS "Data início", 
+    aluguel.Data_Prevista AS "Data prevista", 
+    aluguel.Data_Devolucao AS "Data devolução",
+    pagamento.status AS "Status pagamento"
+FROM Cliente
+INNER JOIN Aluguel ON Cliente.cod_cliente=Aluguel.cod_cliente
+INNER JOIN Pagamento ON Aluguel.cod_aluguel=Pagamento.cod_aluguel
+INNER JOIN Veiculo ON Aluguel.cod_veiculo=Veiculo.cod_veiculo
+INNER JOIN modelo ON veiculo.cod_modelo=modelo.cod_modelo
+INNER JOIN marca ON modelo.Cod_Marca = marca.Cod_Marca;
+
+SELECT 
+	Marca.Nome AS "Marca", 
+    Modelo.Nome AS "Modelo", 
+    COUNT(Veiculo.cod_veiculo) AS "Quantidade em inventário",
+    COUNT(distinct if(Veiculo.status="D", veiculo.cod_veiculo, null)) AS "Quantidade disponível"
+FROM Marca
+INNER JOIN Modelo ON Marca.cod_marca=Modelo.cod_marca
+LEFT JOIN Veiculo ON Modelo.cod_modelo=Veiculo.cod_modelo
+GROUP BY Modelo.nome
+ORDER BY Marca.nome;
